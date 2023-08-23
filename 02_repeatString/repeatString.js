8735c978cb795a3b997c38b940e1383c8ba3e693
Michal <123924536+mmierzwa2002@users.noreply.github.com>
2023-08-23 01:30:25 +0100

    Add solution to exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let resultString = "";
+    if (num < 0) {
+        return "ERROR";
+    } else {
+    for(let i = 0; i < num; i++) {
+        resultString += string;
+    }
+} return resultString;
 };
 
 // Do not edit below this line
