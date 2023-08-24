9e02a3b74d87f6c7bd0b72ff9b2c819c5636b8c2
Michal <123924536+mmierzwa2002@users.noreply.github.com>
2023-08-24 02:55:23 +0100

    Add solution to exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    let smaller = 0;
+    let bigger = 0;
+    let sum = 0;
+    if((x<0 || y<0) || (typeof x != "number" || typeof y != "number")) {
+        return "ERROR";
+    } else if (x > y) {
+        smaller = y;
+        bigger = x;
+    } else {
+        smaller = x;
+        bigger = y;
+    }
 
+    for (smaller; smaller <= bigger; smaller++) {
+        sum += smaller;
+    }
+    return sum;
 };
 
 // Do not edit below this line
