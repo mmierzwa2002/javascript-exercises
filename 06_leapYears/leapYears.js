a65f567fd3e3c6a003df3ed1b4c080636945f9b3
Michal <123924536+mmierzwa2002@users.noreply.github.com>
2023-08-26 21:31:52 +0100

    Add solution to exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //is the year divisible by 400? if yes, it's a leap year
+    if (year % 400 == 0) return true;
+    //is the year divisible by 100? if yes, it's not a leap year
+    if(year % 100 == 0) return false;
+    //is the year divisible by 4? if yes, it's a leap year
+    if (year % 4 == 0) return true;
 
+    //if nothing is checked, it's not a leap year
+
+    return false;
 };
 
 // Do not edit below this line
